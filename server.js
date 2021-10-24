06c332e989cb20ae4fd8843f7cbf969f579f1525
Rohan Singh <rohannsingh98765@gmail.com>
2021-10-24 16:26:49 +0530

    syncing home with server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 const ejs = require('ejs')
-const expressLayout = require('express-js-layouts')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
 
 // if(process.env.PORT) {
 //     PORT.process.env.PORT
@@ -14,10 +15,13 @@ const expressLayout = require('express-js-layouts')
 const PORT = process.env.PORT || 3300;
 
 app.get('/', (req, res) => {
-    res.render("Hello")
+    res.render("home")
 })
 
 //  Set Template Engine
+app.use(expressLayout) 
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
 
 app.listen(PORT ,() => {
     console.log(`listening on port ${PORT}`)
